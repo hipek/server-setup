217773ab7db960df5c339091194eefa573b49c51
Jacek Hiszpanski <jacek.hiszpanski@tooploox.com>
2016-07-09 16:49:39 +0200

    Add consul dep

--- remote-deps/consul.rb
+++ remote-deps/consul.rb
@@ -1,3 +1,15 @@
-dep 'consul' do
+dep 'consul', :host do
+  requires 'consul.bin'.with(host)
+end
 
+dep 'consul.bin', :host do
+  ssh(host) do |r|
+    met? {
+      r.shell 'which consul'
+    }
+
+    meet {
+      p :install_consul
+    }
+  end
 end
