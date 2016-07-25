c520e6669adc15f683cfb80028589eff1345600d
Jacek Hiszpanski <jacek.hiszpanski@tooploox.com>
2016-07-25 21:29:25 +0200

    Start nomad

--- babushka-deps/nomad.rb
+++ babushka-deps/nomad.rb
@@ -1,6 +1,7 @@
 dep 'nomad', :host do
   requires 'nomad.hashicorp'.with(host),
-    'nomad.init'.with(host)
+    'nomad.init'.with(host),
+    'nomad.start'.with(host)
 end
 
 dep 'nomad.hashicorp', :host do
@@ -11,3 +12,5 @@ dep 'nomad.init', :host do
   ip = '127.0.0.1'
   params "agent -dev -bind=#{ip} -network-interface=eth0"
 end
+
+dep 'nomad.start', :host
